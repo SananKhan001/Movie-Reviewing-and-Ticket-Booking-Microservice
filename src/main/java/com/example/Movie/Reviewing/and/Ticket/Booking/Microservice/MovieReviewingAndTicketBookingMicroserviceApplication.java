1a76e5be4fea49bad5740e32661c013686d30aa9
Sanan Khan <sanan3946@gmail.com>
2023-02-26 01:58:05 +0530

    Initial commit

---
+++ src/main/java/com/example/Movie/Reviewing/and/Ticket/Booking/Microservice/MovieReviewingAndTicketBookingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Movie.Reviewing.and.Ticket.Booking.Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewingAndTicketBookingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewingAndTicketBookingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Movie/Reviewing/and/Ticket/Booking/Microservice/MovieReviewingAndTicketBookingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Movie.Reviewing.and.Ticket.Booking.Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewingAndTicketBookingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
