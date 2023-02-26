417a912825275b71f6c758b550ae76f7402b5d8d
Sanan Khan <sanan3946@gmail.com>
2023-02-26 14:41:26 +0530

    Commit inti

--- src/main/java/com/example/Movie/Reviewing/and/Ticket/Booking/Microservice/model/Genre.java
+++ src/main/java/com/example/Movie/Reviewing/and/Ticket/Booking/Microservice/model/Genre.java
@@ -1,4 +1,12 @@
 package com.example.Movie.Reviewing.and.Ticket.Booking.Microservice.model;
 
 public enum Genre {
+
+    ACTION,
+    COMEDY,
+    THRILLER,
+    ROMANCE,
+    SCI_FI,
+    DRAMA,
+    
 }
